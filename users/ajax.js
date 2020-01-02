0a557bbf962613424b271a3629b55a4f33ad222f
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-01-02 16:51:44 +0800

    {feat}-{ajax}: Ajax.js Done

--- users/ajax.js
+++ users/ajax.js
@@ -8,6 +8,26 @@ options = {
   fail: function(error) {}    // 请求失败或出错后调用此方法
 }
 **/
-window.ajax = function (options) {
+window.ajax = function(options) {
+  options = {
+    url: options.url || "",
+    method: options.method || "get",
+    headers: options.headers || {},
+    data: options.data || null,
+    onSuccess: options.success || function(result) {},
+    onFail: options.fail || function(result) {}
+  };
 
-}
+  var xhr = new XMLHttpRequest();
+  xhr.open(options.method, options.url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        options.onSuccess(xhr.responseText);
+      } else {
+        options.onFail(xhr.status);
+      }
+    }
+  };
+  xhr.send(options.data);
+};
--- users/index.js
+++ users/index.js
@@ -1,82 +1,87 @@
-(function () {
-    const API_ROOT = "http://localhost:3000/users";
-    let $userList = document.getElementById('user-list');
-    let $username = document.getElementById('username');
-    let $password = document.getElementById('password');
+(function() {
+  const API_ROOT = "http://localhost:3000/users";
+  let $userList = document.getElementById("user-list");
+  let $username = document.getElementById("username");
+  let $password = document.getElementById("password");
 
-    // TODO： get user list，when success, run renderUserList
-    function getListData() {
+  // TODO： get user list，when success, run renderUserList
+  function getListData() {
+      options = {
+          url: API_ROOT,
+          method: "get",
+          success: function(result){
+              renderUserList(result);
+          }
+      }
+      ajax(options);
+  }
 
+  // TODO:  add user info,when success, run addItem
+  function addItemData(data) {}
+
+  // TODO: update user info,when success, run updateItem
+  // 提示：Math.random().toString(36).substring(2) 生成随机的字符串
+  function updateItemData(id) {}
+
+  // TODO: delete user info,,when success, run  deleteItem
+  function deleteItemData(id) {}
+
+  document.getElementById("add-btn").addEventListener("click", function() {
+    let $username = document.getElementById("username").value;
+    let $password = document.getElementById("password").value;
+    let data = {
+      username: $username, // Math.random().toString(36).substring(2),
+      password: $password
+    };
+    addItemData(data);
+  });
+
+  $userList.addEventListener("click", function(event) {
+    let id =
+      event.target.getAttribute("data-id") ||
+      event.target.parentElement.getAttribute("data-id");
+
+    switch (true) {
+      case event.target.innerHTML === "X":
+        deleteItemData(id);
+        break;
+      default:
+        updateItemData(id);
+    }
+  });
+
+  function renderUserList(data) {
+    if (!Array.isArray(data) && !data instanceof Array) {
+      return false;
     }
 
-    // TODO:  add user info,when success, run addItem
-    function addItemData(data) {
+    // TODO Should I add this parse statement here?
+    data = JSON.parse(data);
 
-    }
+    // TODO What is acc?
+    $userList.innerHTML = data.reduce((acc, cur) => {
+      return (acc += `<li data-id="${cur.id}"><span>用户名：${cur.username} - 密码：${cur.password}</span><span>X</span></li>`);
+    }, "");
+  }
 
-    // TODO: update user info,when success, run updateItem
-    // 提示：Math.random().toString(36).substring(2) 生成随机的字符串
-    function updateItemData(id) {
+  function addItem(item) {
+    var $item = document.createElement("li");
+    $item.setAttribute("data-id", item.id);
+    $item.innerHTML = `<span>用户名：${item.username} - 密码：${item.password}</span><span>X</span>`;
+    $userList.appendChild($item);
+    $username.value = "";
+    $password.value = "";
+  }
 
-    }
+  function updateItem(item) {
+    var $item = $userList.querySelector(`li[data-id='${item.id}']`);
+    $item.innerHTML = `<span>用户名：${item.username} - 密码：${item.password}</span><span>X</span>`;
+  }
 
-    // TODO: delete user info,,when success, run  deleteItem
-    function deleteItemData(id) {
+  function deleteItem(id) {
+    var $item = $userList.querySelector(`li[data-id='${id}']`);
+    $userList.removeChild($item);
+  }
 
-    }
-
-    document.getElementById('add-btn').addEventListener('click', function () {
-        let $username = document.getElementById('username').value;
-        let $password = document.getElementById('password').value;
-        let data = {
-            "username": $username, // Math.random().toString(36).substring(2),
-            "password": $password
-        }
-        addItemData(data);
-    })
-
-    $userList.addEventListener('click', function (event) {
-        let id = event.target.getAttribute('data-id') || event.target.parentElement.getAttribute('data-id');
-
-        switch (true) {
-            case event.target.innerHTML === 'X':
-                deleteItemData(id);
-                break;
-            default:
-                updateItemData(id);
-        }
-    })
-
-    function renderUserList(data) {
-        if (!Array.isArray(data) && !data instanceof Array) {
-            return false;
-        }
-
-        $userList.innerHTML = data.reduce((acc, cur) => {
-            return acc += `<li data-id="${cur.id}"><span>用户名：${cur.username} - 密码：${cur.password}</span><span>X</span></li>`;
-        }, '');
-
-    }
-
-    function addItem(item) {
-        var $item = document.createElement('li');
-        $item.setAttribute('data-id', item.id);
-        $item.innerHTML = `<span>用户名：${item.username} - 密码：${item.password}</span><span>X</span>`;
-        $userList.appendChild($item);
-        $username.value = '';
-        $password.value = '';
-    }
-
-    function updateItem(item) {
-        var $item = $userList.querySelector(`li[data-id='${item.id}']`);
-        $item.innerHTML = `<span>用户名：${item.username} - 密码：${item.password}</span><span>X</span>`;
-    }
-
-    function deleteItem(id) {
-        var $item = $userList.querySelector(`li[data-id='${id}']`);
-        $userList.removeChild($item);
-    }
-
-    getListData();
-
+  getListData();
 })();
